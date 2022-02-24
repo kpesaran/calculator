b6f5188c3ae525f03a45c5dc881b6cbfd0c68dbb
Keyan <Kpesaran@gmail.com>
2022-02-23 22:37:48 -0700

    Stylistic changes and fixed bugs associated with delete button

--- script-calculator.js
+++ script-calculator.js
@@ -9,24 +9,26 @@ let arrayDataType = []
 
 
 calcButton.addEventListener('click', (event) => {
-    //if (!event.target.closest('button')) return;
-    console.log(event)
+   
+    //For specific button clicked
     const currentB = event.target;
     buttonValue = currentB.textContent;
 
+    //variable that stores current type, pushes it to array so that previous key type is stored.
     const { type } = currentB.dataset;
-    arrayDataType.push(type)
+    if (type !== undefined) {
+        arrayDataType.push(type)
+    }
+    
     const previousKeyType = arrayDataType[arrayDataType.length - 2]
-
     
     let displayEquation = userInput.textContent;
-    // When first getting to page
+    // allows user to connect to numbers 
     if (type === 'number' && (previousKeyType === 'number' || previousKeyType === undefined || previousKeyType==='backspace')) {
         userInput.textContent += buttonValue
         displayEquation += buttonValue
     }
-
- 
+    // adds space between number and operator
     if (type === 'number' && previousKeyType === 'operator') {
         userInput.textContent += ' ' + buttonValue
     }
@@ -34,23 +36,47 @@ calcButton.addEventListener('click', (event) => {
     if (type === 'operator' && (previousKeyType === 'number'|| previousKeyType === 'backspace' )) {
         userInput.textContent = displayEquation + ' ' + buttonValue
     }
+    if (type === 'number' && previousKeyType === 'backspace') {
+        displayEquation = userInput.textContent + buttonValue
+    }
 
     if (type === 'reset') {
         userInput.textContent = ''
+        result.textContent = '0'
         arrayDataType = []
     }
+
+
     if (type === 'backspace') {
         if (previousKeyType ==='operator') {
             userInput.textContent = displayEquation.substring(0, displayEquation.length -2)
         }
         else if (previousKeyType === 'number') {
             userInput.textContent = displayEquation.substring(0, displayEquation.length - 1)
-            arrayDataType = []
+        }
+        else if (previousKeyType === 'backspace') {
+            if (displayEquation.endsWith('+') || displayEquation.endsWith('-') || displayEquation.endsWith('÷') || displayEquation.endsWith('×') || displayEquation.endsWith(' ')) {
+                userInput.textContent = displayEquation.substring(0, displayEquation.length -2)
+            }
+        
+            else  {
+                userInput.textContent = displayEquation.substring(0, displayEquation.length - 1)
+            }
         }
     }
     if (type === 'equals') {
         result.textContent = convertToEquation(expression)
-    }    
+        elementArray = [result.textContent]
+    }
+    
+    if (previousKeyType === 'equals' && type === 'operator')
+    {
+
+        userInput.textContent = result.textContent + ' ' + buttonValue
+    }
+    
+    console.log(arrayDataType)
+    console.log(displayEquation)
 });
 
 function evaluate(firstNumber, currentOperator, secondNumber) {
@@ -66,6 +92,14 @@ function evaluate(firstNumber, currentOperator, secondNumber) {
         return resultValue
     }
     if (currentOperator === '÷') {
+        if (secondNumber === '0') {
+            if (Number(firstNumber) > 0)
+                return 'Infinity'
+            else if (Number(firstNumber === 0)) {
+                return 0
+            }
+            else return '-Infinity'
+        }
         let resultValue = Number(firstNumber) / Number(secondNumber);
         console.log(resultValue)
         return resultValue
